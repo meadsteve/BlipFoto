b10f88d3615eb67633a7d5239ce3796f5a6bdfa8
meadsteve <meadsteve@gmail.com>
2013-02-03 21:50:25 +0000

    Added fluency to the SearchRequest class.

--- src/MeadSteve/BlipFoto/Requests/SearchRequest.php
+++ src/MeadSteve/BlipFoto/Requests/SearchRequest.php
@@ -30,6 +30,7 @@ class SearchRequest
 	/**
 	 * Sets the query for the search
 	 * @param string $searchPhrase
+	 * @return SearchRequest
 	 * @throws \InvalidArgumentException if $searchPhrase is invalid
 	 */
 	public function query($searchPhrase) {
@@ -37,11 +38,14 @@ class SearchRequest
 			throw new \InvalidArgumentException('$searchPhrase must be a string');
 		}
 		$this->requestQuery->add('query', $searchPhrase);
+
+		return $this;
 	}
 
 	/**
 	 * Sets the maximum number of results to return.
 	 * @param integer $maxResults
+	 * @return SearchRequest
 	 * @throws \InvalidArgumentException
 	 */
 	public function maxResults($maxResults) {
@@ -49,23 +53,31 @@ class SearchRequest
 			throw new \InvalidArgumentException('$maxResults must be an integer');
 		}
 		$this->requestQuery->add('max', $maxResults);
+
+		return $this;
 	}
 
 	/**
 	 * Sets the size of the returned thumbnails. Two constants are provided:
 	 * THUMBNAIL_SIZE_Big and THUMBNAIL_SIZE_Small
 	 * @param $size
+	 * @return SearchRequest
 	 */
 	public function size($size) {
 		$this->requestQuery->add('size', $size);
+
+		return $this;
 	}
 
 	/**
 	 * Sets the size of the returned thumbnails. Two constants are provided:
 	 * THUMBNAIL_COLOR_Color and THUMBNAIL_COLOR_Grey
 	 * @param $color
+	 * @return SearchRequest
 	 */
 	public function color($color) {
 		$this->requestQuery->add('color', $color);
+
+		return $this;
 	}
 }
